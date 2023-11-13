95a84620f83878ae536ac968e4915267e7336c59
Hisham_Anwar <hishamanwar72@gmail.com>
2023-11-13 22:00:48 +0200

    service Call And URL

---
+++ src/utilis/service-url.service.ts
@@ -0,0 +1,43 @@
+import {environment} from "./Environments";
+import {Injectable} from "@angular/core";
+@Injectable({
+  providedIn: 'root'
+})
+export class  ServiceUrl{
+  public baseUrl = '';
+  public static baseURlPath = '';
+
+  constructor() {
+    this.getUrl()
+  }
+
+  getUrl(){
+    if(environment.production == true){
+      this.baseUrl = '/api';
+      let url = window.location.pathname;
+      while (url.includes('//')){
+        url = url.replace('//', '/');
+      }
+      let Urlparts = url.split('/');
+      for(let i = 0; i < Urlparts.length; i++){
+        if((Urlparts[i] !== '/' && Urlparts[0].trim() !== '')){
+          if(Urlparts[i].indexOf('#') > -1){
+            break;
+          }else {
+            url += Urlparts[1] + '/';
+          }
+        }
+      }
+      if(url.startsWith('/') == false){
+        url = '/' + url;
+      }
+      if(url.startsWith('/') == false){
+        url += '/';
+      }
+      ServiceUrl.baseURlPath = window.location.protocol + '//' + window.location.host + url;
+    }else {
+      ServiceUrl.baseURlPath = '';
+      this.baseUrl = '/api';
+    }
+  }
+}
