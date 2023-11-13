f107aa5945992b19e1511705829b4ae92948bbe5
Hisham_Anwar <hishamanwar72@gmail.com>
2023-11-13 22:31:16 +0200

    error page && login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    'path': 'auth', children: [
+      {'path': 'login', loadComponent: () => (import('./auth/login/login.component').then(m=>m.LoginComponent))},
+      {'path': '', 'pathMatch': "full", redirectTo: '/auth/login'}
+    ]
+  },
+  {'path': '', 'pathMatch': "full", redirectTo: "/auth/login"},
+  {'path': '**', 'pathMatch': 'full', loadComponent: () => (import('./common/error-page/error-page.component').then(m=>m.ErrorPageComponent))}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
